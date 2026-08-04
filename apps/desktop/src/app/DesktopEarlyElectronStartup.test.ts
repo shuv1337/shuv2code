0bkevin/t3code

fix(desktop): Niri/Hyprland - Linux secret storage backend (#2916)
Co-authored-by: Julius Marminge <julius0216@outlook.com>

apps/desktop/src/app/DesktopEarlyElectronStartup.test.ts
@@ -1,0 +1,121 @@
+// @effect-diagnostics nodeBuiltinImport:off - tests use POSIX path joining to match the Linux startup boundary.
+import * as NodePath from "node:path";
+import { assert, describe, it } from "@effect/vitest";
+
+import {
+  resolveEarlyLinuxElectronOptions,
+  resolveEarlyLinuxPasswordStorePreference,
+} from "./DesktopEarlyElectronStartup.ts";
+
+describe("DesktopEarlyElectronStartup", () => {
+  const joinPath = NodePath.posix.join;
+
+  it("reads the persisted linux password-store preference before Electron is ready", () => {
+    const preference = resolveEarlyLinuxPasswordStorePreference({
+      env: { T3CODE_HOME: "/home/user/.t3-test" },
+      homeDirectory: "/home/user",
+      joinPath,
+      readFileString: (path) => {
+        assert.equal(path, "/home/user/.t3-test/userdata/desktop-settings.json");
+        return JSON.stringify({ linuxPasswordStore: "kwallet6" });
+      },
+    });
+
+    assert.equal(preference, "kwallet6");
+  });
+
+  it("accepts JSONC in the early desktop settings file", () => {
+    const preference = resolveEarlyLinuxPasswordStorePreference({
+      env: { T3CODE_HOME: "/home/user/.t3-test" },
+      homeDirectory: "/home/user",
+      joinPath,
+      readFileString: () => `{
+        // manually edited setting
+        "linuxPasswordStore": "gnome-libsecret",
+      }`,
+    });
+
+    assert.equal(preference, "gnome-libsecret");
+  });
+
+  it("falls back to auto when the early settings document is missing or invalid", () => {
+    const preference = resolveEarlyLinuxPasswordStorePreference({
+      env: {},
+      homeDirectory: "/home/user",
+      joinPath,
+      readFileString: () => {
+        throw new Error("missing");
+      },
+    });
+
+    assert.equal(preference, "auto");
+  });
+
+  it("preserves absolute root paths when resolving early settings", () => {
+    const preference = resolveEarlyLinuxPasswordStorePreference({
+      env: { T3CODE_HOME: "/" },
+      homeDirectory: "/home/user",
+      joinPath,
+      readFileString: (path) => {
+        assert.equal(path, "/userdata/desktop-settings.json");
+        return JSON.stringify({ linuxPasswordStore: "kwallet6" });
+      },
+    });
+
+    assert.equal(preference, "kwallet6");
+  });
+
+  it("resolves the early linux Electron switches", () => {
+    const options = resolveEarlyLinuxElectronOptions({
+      env: {
+        T3CODE_HOME: "/home/user/.t3-test",
+        XDG_CURRENT_DESKTOP: "niri",
+        VITE_DEV_SERVER_URL: "http://127.0.0.1:5173",
+      },
+      homeDirectory: "/home/user",
+      joinPath,
+      readFileString: (path) => {
+        assert.equal(path, "/home/user/.t3-test/userdata/desktop-settings.json");
+        return JSON.stringify({ linuxPasswordStore: "auto" });
+      },
+    });
+
+    assert.deepEqual(options, {
+      linuxWmClass: "t3code-dev",
+      passwordStore: "gnome-libsecret",
+    });
+  });
+
+  it("keeps implicit development state under ~/.t3/dev when T3CODE_HOME is unset", () => {
+    const preference = resolveEarlyLinuxPasswordStorePreference({
+      env: {
+        VITE_DEV_SERVER_URL: "http://127.0.0.1:5173",
+      },
+      homeDirectory: "/home/user",
+      joinPath,
+      readFileString: (path) => {
+        assert.equal(path, "/home/user/.t3/dev/desktop-settings.json");
+        return JSON.stringify({ linuxPasswordStore: "kwallet" });
+      },
+    });
+
+    assert.equal(preference, "kwallet");
+  });
+
+  it("treats whitespace-only T3CODE_HOME as unconfigured in development", () => {
+    const preference = resolveEarlyLinuxPasswordStorePreference({
+      env: {
+        T3CODE_HOME: "   ",
+        VITE_DEV_SERVER_URL: "http://127.0.0.1:5173",
+      },
+      homeDirectory: "/home/user",
+      joinPath,
+      readFileString: (path) => {
+        assert.equal(path, "/home/user/.t3/dev/desktop-settings.json");
+        return JSON.stringify({ linuxPasswordStore: "gnome-libsecret" });
+      },
+    });
+
+    assert.equal(preference, "gnome-libsecret");
+  });
+});

apps/desktop/src/app/DesktopEarlyElectronStartup.ts
@@ -1,0 +1,90 @@
+import { fromLenientJson } from "@t3tools/shared/schemaJson";
+import * as Option from "effect/Option";
+import * as Schema from "effect/Schema";
+
+import {
+  DEFAULT_LINUX_PASSWORD_STORE,
+  normalizeLinuxPasswordStorePreference,
+  resolveLinuxPasswordStoreSwitch,
+  type LinuxPasswordStoreSwitch,
+  type LinuxPasswordStorePreference,
+} from "../linuxSecretStorage.ts";
+import {
+  resolveDesktopBaseDir,
+  resolveDesktopStateDir,
+  type JoinPath,
+} from "./DesktopStatePaths.ts";
+
+interface EarlyDesktopSettingsInput {
+  readonly env: NodeJS.ProcessEnv;
+  readonly homeDirectory: string;
+  readonly joinPath: JoinPath;
+  readonly readFileString: (path: string) => string;
+}
+
+type EarlyLinuxElectronOptionsInput = EarlyDesktopSettingsInput;
+
+export interface EarlyLinuxElectronOptions {
+  readonly linuxWmClass: string;
+  readonly passwordStore: LinuxPasswordStoreSwitch | null;
+}
+
+const trimNonEmpty = (value: string | undefined): string | null => {
+  const trimmed = value?.trim();
+  return trimmed && trimmed.length > 0 ? trimmed : null;
+};
+
+const EarlyDesktopSettingsJson = fromLenientJson(
+  Schema.Struct({
+    linuxPasswordStore: Schema.optionalKey(Schema.Unknown),
+  }),
+);
+const decodeEarlyDesktopSettingsJson = Schema.decodeSync(EarlyDesktopSettingsJson);
+
+const isDevelopmentEnvironment = (env: NodeJS.ProcessEnv): boolean =>
+  trimNonEmpty(env.VITE_DEV_SERVER_URL) !== null;
+
+function resolveEarlyDesktopSettingsPath(input: {
+  readonly env: NodeJS.ProcessEnv;
+  readonly homeDirectory: string;
+  readonly joinPath: JoinPath;
+}): string {
+  const t3Home = Option.fromUndefinedOr(input.env.T3CODE_HOME);
+  const baseDir = resolveDesktopBaseDir({
+    homeDirectory: input.homeDirectory,
+    joinPath: input.joinPath,
+    t3Home,
+  });
+  const stateDir = resolveDesktopStateDir({
+    baseDir,
+    isDevelopment: isDevelopmentEnvironment(input.env),
+    joinPath: input.joinPath,
+    t3Home,
+  });
+  return input.joinPath(stateDir, "desktop-settings.json");
+}
+
+export function resolveEarlyLinuxPasswordStorePreference(
+  input: EarlyDesktopSettingsInput,
+): LinuxPasswordStorePreference {
+  const settingsPath = resolveEarlyDesktopSettingsPath(input);
+  try {
+    const parsed = decodeEarlyDesktopSettingsJson(input.readFileString(settingsPath));
+    return normalizeLinuxPasswordStorePreference(parsed.linuxPasswordStore);
+  } catch {
+    return DEFAULT_LINUX_PASSWORD_STORE;
+  }
+}
+
+export function resolveEarlyLinuxElectronOptions(
+  input: EarlyLinuxElectronOptionsInput,
+): EarlyLinuxElectronOptions {
+  const preference = resolveEarlyLinuxPasswordStorePreference(input);
+  return {
+    linuxWmClass: isDevelopmentEnvironment(input.env) ? "t3code-dev" : "t3code",
+    passwordStore: resolveLinuxPasswordStoreSwitch({
+      preference,
+      env: input.env,
+    }),
+  };
+}

apps/desktop/src/app/DesktopPreReadyPlatform.test.ts
@@ -1,0 +1,130 @@
+import { assert, describe, it } from "@effect/vitest";
+import { HostProcessPlatform } from "@t3tools/shared/hostProcess";
+import * as Context from "effect/Context";
+import * as Effect from "effect/Effect";
+import * as Layer from "effect/Layer";
+import { beforeEach, vi } from "vite-plus/test";
+
+const { appendSwitchMock, getSwitchValueMock, hasSwitchMock, registerSchemesMock } = vi.hoisted(
+  () => ({
+    appendSwitchMock: vi.fn(),
+    getSwitchValueMock: vi.fn(),
+    hasSwitchMock: vi.fn(),
+    registerSchemesMock: vi.fn(),
+  }),
+);
+
+vi.mock("electron", () => ({
+  app: {
+    commandLine: {
+      appendSwitch: appendSwitchMock,
+      getSwitchValue: getSwitchValueMock,
+      hasSwitch: hasSwitchMock,
+    },
+  },
+  protocol: {
+    registerSchemesAsPrivileged: registerSchemesMock,
+  },
+}));
+
+import * as DesktopPreReadyPlatform from "./DesktopPreReadyPlatform.ts";
+
+describe("DesktopPreReadyPlatform", () => {
+  beforeEach(() => {
+    appendSwitchMock.mockReset();
+    getSwitchValueMock.mockReset();
+    hasSwitchMock.mockReset();
+    registerSchemesMock.mockReset();
+  });
+
+  it("reads an explicit Electron command-line switch value", () => {
+    const value = DesktopPreReadyPlatform.readCommandLineSwitchValue(
+      {
+        hasSwitch: (switchName) => switchName === "password-store",
+        getSwitchValue: (switchName) => {
+          assert.equal(switchName, "password-store");
+          return "basic";
+        },
+      },
+      "password-store",
+    );
+
+    assert.equal(value, "basic");
+  });
+
+  it("treats valueless Electron command-line switches as absent", () => {
+    const value = DesktopPreReadyPlatform.readCommandLineSwitchValue(
+      {
+        hasSwitch: () => true,
+        getSwitchValue: () => "",
+      },
+      "password-store",
+    );
+
+    assert.isNull(value);
+  });
+
+  it("returns null for missing Electron command-line switches", () => {
+    const value = DesktopPreReadyPlatform.readCommandLineSwitchValue(
+      {
+        hasSwitch: () => false,
+        getSwitchValue: () => {
+          throw new Error("Unexpected switch value read.");
+        },
+      },
+      "password-store",
+    );
+
+    assert.isNull(value);
+  });
+
+  it.effect(
+    "acquires a synchronous pre-ready layer before an asynchronous Clerk-shaped layer",
+    () =>
+      Effect.gen(function* () {
+        class ClerkShaped extends Context.Service<ClerkShaped, { readonly ready: true }>()(
+          "@t3tools/desktop/app/DesktopPreReadyPlatform.test/ClerkShaped",
+        ) {}
+
+        const events: Array<string> = [];
+        registerSchemesMock.mockImplementation(() => {
+          events.push("pre-ready");
+        });
+
+        const preReadyLayer = DesktopPreReadyPlatform.layer.pipe(
+          Layer.provide(Layer.succeed(HostProcessPlatform, "darwin")),
+        );
+
+        const clerkShapedLayer = Layer.effect(
+          ClerkShaped,
+          Effect.promise(() => Promise.resolve()).pipe(
+            Effect.map(() => {
+              events.push("clerk");
+              return { ready: true as const };
+            }),
+          ),
+        );
+
+        const runtimeLayer = clerkShapedLayer.pipe(
+          Layer.flatMap((clerkContext) => Layer.succeedContext(clerkContext)),
+          Layer.provideMerge(preReadyLayer),
+        );
+
+        const result = yield* Effect.all({
+          clerk: ClerkShaped,
+          preReady: DesktopPreReadyPlatform.DesktopPreReadyElectronOptions,
+        }).pipe(Effect.provide(runtimeLayer));
+
+        assert.deepEqual(result, {
+          clerk: { ready: true },
+          preReady: {
+            linux: null,
+            linuxPasswordStoreCommandLine: null,
+          },
+        });
+        assert.deepEqual(events, ["pre-ready", "clerk"]);
+        assert.equal(registerSchemesMock.mock.calls.length, 1);
+        assert.equal(appendSwitchMock.mock.calls.length, 0);
+      }),
+  );
+});

apps/desktop/src/electron/ElectronSafeStorage.ts
@@ -1,9 +1,11 @@
 import * as Context from "effect/Context";
 import * as Effect from "effect/Effect";
 import * as Layer from "effect/Layer";
+import * as Option from "effect/Option";
 import * as Schema from "effect/Schema";
 
 import * as Electron from "electron";
+import { HostProcessPlatform } from "@t3tools/shared/hostProcess";
 
 const electronSafeStorageErrorFields = {
   cause: Schema.Defect(),
@@ -60,24 +62,39 @@ export class ElectronSafeStorage extends Context.Service<
     readonly decryptString: (
       value: Uint8Array,
     ) => Effect.Effect<string, ElectronSafeStorageDecryptError>;
+    readonly selectedStorageBackend: Effect.Effect<Option.Option<string>>;
   }
 >()("@t3tools/desktop/electron/ElectronSafeStorage") {}
 
-export const make = ElectronSafeStorage.of({
-  isEncryptionAvailable: Effect.try({
-    try: () => Electron.safeStorage.isEncryptionAvailable(),
-    catch: (cause) => new ElectronSafeStorageAvailabilityError({ cause }),
-  }),
-  encryptString: (value) =>
-    Effect.try({
-      try: () => Electron.safeStorage.encryptString(value),
-      catch: (cause) => new ElectronSafeStorageEncryptError({ cause }),
+export const make = Effect.gen(function* () {
+  const platform = yield* HostProcessPlatform;
+
+  return ElectronSafeStorage.of({
+    isEncryptionAvailable: Effect.try({
+      try: () => Electron.safeStorage.isEncryptionAvailable(),
+      catch: (cause) => new ElectronSafeStorageAvailabilityError({ cause }),
     }),
-  decryptString: (value) =>
-    Effect.try({
-      try: () => Electron.safeStorage.decryptString(Buffer.from(value)),
-      catch: (cause) => new ElectronSafeStorageDecryptError({ cause }),
+    encryptString: (value) =>
+      Effect.try({
+        try: () => Electron.safeStorage.encryptString(value),
+        catch: (cause) => new ElectronSafeStorageEncryptError({ cause }),
+      }),
+    decryptString: (value) =>
+      Effect.try({
+        try: () => Electron.safeStorage.decryptString(Buffer.from(value)),
+        catch: (cause) => new ElectronSafeStorageDecryptError({ cause }),
+      }),
+    selectedStorageBackend: Effect.sync(() => {
+      if (platform !== "linux") {
+        return Option.none();
+      }
+      try {
+        return Option.fromNullishOr(Electron.safeStorage.getSelectedStorageBackend());
+      } catch {
+        return Option.none();
+      }
     }),
+  });
 });
 
-export const layer = Layer.succeed(ElectronSafeStorage, make);
+export const layer = Layer.effect(ElectronSafeStorage, make);

shuv1337/shuv2code

Ported from upstream pingdotgg/t3code#2916 (6f04a5cff).
Chromium only selects a real keyring backend when XDG_CURRENT_DESKTOP
matches a known desktop, so on Hyprland/Niri/Sway sessions Electron lands
on basic_text and safeStorage reports encryption unavailable — pairing an
environment fails with "Desktop secure storage is unavailable in this
system context" even when GNOME Keyring is running. Force
--password-store=gnome-libsecret pre-ready for sessions Electron would
leave unprotected, honor an explicit linuxPasswordStore desktop setting
and any user-provided --password-store switch, import session/dbus env
from the login shell, and log the selected backend after ready.
Fork adaptations:
- rebranded identifiers (@t3tools -> @shuv2code, T3CODE_HOME ->
  SHUV2CODE_HOME, ~/.t3 -> ~/.shuv2code, t3code wm classes/messages)
- kept this fork's scheme privileges and moved its existing synchronous
  registerDesktopSchemesAsPrivileged() call from main.ts into the new
  pre-ready layer instead of adding upstream's duplicate registration
- dropped upstream's linuxApplicationsDir hunk (belongs to #2915, not
  merged here)

apps/desktop/src/app/DesktopEarlyElectronStartup.test.ts
@@ -1,0 +1,121 @@
+// @effect-diagnostics nodeBuiltinImport:off - tests use POSIX path joining to match the Linux startup boundary.
+import * as NodePath from "node:path";
+import { assert, describe, it } from "@effect/vitest";
+
+import {
+  resolveEarlyLinuxElectronOptions,
+  resolveEarlyLinuxPasswordStorePreference,
+} from "./DesktopEarlyElectronStartup.ts";
+
+describe("DesktopEarlyElectronStartup", () => {
+  const joinPath = NodePath.posix.join;
+
+  it("reads the persisted linux password-store preference before Electron is ready", () => {
+    const preference = resolveEarlyLinuxPasswordStorePreference({
+      env: { SHUV2CODE_HOME: "/home/user/.shuv2code-test" },
+      homeDirectory: "/home/user",
+      joinPath,
+      readFileString: (path) => {
+        assert.equal(path, "/home/user/.shuv2code-test/userdata/desktop-settings.json");
+        return JSON.stringify({ linuxPasswordStore: "kwallet6" });
+      },
+    });
+
+    assert.equal(preference, "kwallet6");
+  });
+
+  it("accepts JSONC in the early desktop settings file", () => {
+    const preference = resolveEarlyLinuxPasswordStorePreference({
+      env: { SHUV2CODE_HOME: "/home/user/.shuv2code-test" },
+      homeDirectory: "/home/user",
+      joinPath,
+      readFileString: () => `{
+        // manually edited setting
+        "linuxPasswordStore": "gnome-libsecret",
+      }`,
+    });
+
+    assert.equal(preference, "gnome-libsecret");
+  });
+
+  it("falls back to auto when the early settings document is missing or invalid", () => {
+    const preference = resolveEarlyLinuxPasswordStorePreference({
+      env: {},
+      homeDirectory: "/home/user",
+      joinPath,
+      readFileString: () => {
+        throw new Error("missing");
+      },
+    });
+
+    assert.equal(preference, "auto");
+  });
+
+  it("preserves absolute root paths when resolving early settings", () => {
+    const preference = resolveEarlyLinuxPasswordStorePreference({
+      env: { SHUV2CODE_HOME: "/" },
+      homeDirectory: "/home/user",
+      joinPath,
+      readFileString: (path) => {
+        assert.equal(path, "/userdata/desktop-settings.json");
+        return JSON.stringify({ linuxPasswordStore: "kwallet6" });
+      },
+    });
+
+    assert.equal(preference, "kwallet6");
+  });
+
+  it("resolves the early linux Electron switches", () => {
+    const options = resolveEarlyLinuxElectronOptions({
+      env: {
+        SHUV2CODE_HOME: "/home/user/.shuv2code-test",
+        XDG_CURRENT_DESKTOP: "niri",
+        VITE_DEV_SERVER_URL: "http://127.0.0.1:5173",
+      },
+      homeDirectory: "/home/user",
+      joinPath,
+      readFileString: (path) => {
+        assert.equal(path, "/home/user/.shuv2code-test/userdata/desktop-settings.json");
+        return JSON.stringify({ linuxPasswordStore: "auto" });
+      },
+    });
+
+    assert.deepEqual(options, {
+      linuxWmClass: "shuv2code-dev",
+      passwordStore: "gnome-libsecret",
+    });
+  });
+
+  it("keeps implicit development state under ~/.shuv2code/dev when SHUV2CODE_HOME is unset", () => {
+    const preference = resolveEarlyLinuxPasswordStorePreference({
+      env: {
+        VITE_DEV_SERVER_URL: "http://127.0.0.1:5173",
+      },
+      homeDirectory: "/home/user",
+      joinPath,
+      readFileString: (path) => {
+        assert.equal(path, "/home/user/.shuv2code/dev/desktop-settings.json");
+        return JSON.stringify({ linuxPasswordStore: "kwallet" });
+      },
+    });
+
+    assert.equal(preference, "kwallet");
+  });
+
+  it("treats whitespace-only SHUV2CODE_HOME as unconfigured in development", () => {
+    const preference = resolveEarlyLinuxPasswordStorePreference({
+      env: {
+        SHUV2CODE_HOME: "   ",
+        VITE_DEV_SERVER_URL: "http://127.0.0.1:5173",
+      },
+      homeDirectory: "/home/user",
+      joinPath,
+      readFileString: (path) => {
+        assert.equal(path, "/home/user/.shuv2code/dev/desktop-settings.json");
+        return JSON.stringify({ linuxPasswordStore: "gnome-libsecret" });
+      },
+    });
+
+    assert.equal(preference, "gnome-libsecret");
+  });
+});

apps/desktop/src/app/DesktopEarlyElectronStartup.ts
@@ -1,0 +1,90 @@
+import { fromLenientJson } from "@shuv2code/shared/schemaJson";
+import * as Option from "effect/Option";
+import * as Schema from "effect/Schema";
+
+import {
+  DEFAULT_LINUX_PASSWORD_STORE,
+  normalizeLinuxPasswordStorePreference,
+  resolveLinuxPasswordStoreSwitch,
+  type LinuxPasswordStoreSwitch,
+  type LinuxPasswordStorePreference,
+} from "../linuxSecretStorage.ts";
+import {
+  resolveDesktopBaseDir,
+  resolveDesktopStateDir,
+  type JoinPath,
+} from "./DesktopStatePaths.ts";
+
+interface EarlyDesktopSettingsInput {
+  readonly env: NodeJS.ProcessEnv;
+  readonly homeDirectory: string;
+  readonly joinPath: JoinPath;
+  readonly readFileString: (path: string) => string;
+}
+
+type EarlyLinuxElectronOptionsInput = EarlyDesktopSettingsInput;
+
+export interface EarlyLinuxElectronOptions {
+  readonly linuxWmClass: string;
+  readonly passwordStore: LinuxPasswordStoreSwitch | null;
+}
+
+const trimNonEmpty = (value: string | undefined): string | null => {
+  const trimmed = value?.trim();
+  return trimmed && trimmed.length > 0 ? trimmed : null;
+};
+
+const EarlyDesktopSettingsJson = fromLenientJson(
+  Schema.Struct({
+    linuxPasswordStore: Schema.optionalKey(Schema.Unknown),
+  }),
+);
+const decodeEarlyDesktopSettingsJson = Schema.decodeSync(EarlyDesktopSettingsJson);
+
+const isDevelopmentEnvironment = (env: NodeJS.ProcessEnv): boolean =>
+  trimNonEmpty(env.VITE_DEV_SERVER_URL) !== null;
+
+function resolveEarlyDesktopSettingsPath(input: {
+  readonly env: NodeJS.ProcessEnv;
+  readonly homeDirectory: string;
+  readonly joinPath: JoinPath;
+}): string {
+  const shuv2codeHome = Option.fromUndefinedOr(input.env.SHUV2CODE_HOME);
+  const baseDir = resolveDesktopBaseDir({
+    homeDirectory: input.homeDirectory,
+    joinPath: input.joinPath,
+    shuv2codeHome,
+  });
+  const stateDir = resolveDesktopStateDir({
+    baseDir,
+    isDevelopment: isDevelopmentEnvironment(input.env),
+    joinPath: input.joinPath,
+    shuv2codeHome,
+  });
+  return input.joinPath(stateDir, "desktop-settings.json");
+}
+
+export function resolveEarlyLinuxPasswordStorePreference(
+  input: EarlyDesktopSettingsInput,
+): LinuxPasswordStorePreference {
+  const settingsPath = resolveEarlyDesktopSettingsPath(input);
+  try {
+    const parsed = decodeEarlyDesktopSettingsJson(input.readFileString(settingsPath));
+    return normalizeLinuxPasswordStorePreference(parsed.linuxPasswordStore);
+  } catch {
+    return DEFAULT_LINUX_PASSWORD_STORE;
+  }
+}
+
+export function resolveEarlyLinuxElectronOptions(
+  input: EarlyLinuxElectronOptionsInput,
+): EarlyLinuxElectronOptions {
+  const preference = resolveEarlyLinuxPasswordStorePreference(input);
+  return {
+    linuxWmClass: isDevelopmentEnvironment(input.env) ? "shuv2code-dev" : "shuv2code",
+    passwordStore: resolveLinuxPasswordStoreSwitch({
+      preference,
+      env: input.env,
+    }),
+  };
+}

apps/desktop/src/app/DesktopPreReadyPlatform.test.ts
@@ -1,0 +1,130 @@
+import { assert, describe, it } from "@effect/vitest";
+import { HostProcessPlatform } from "@shuv2code/shared/hostProcess";
+import * as Context from "effect/Context";
+import * as Effect from "effect/Effect";
+import * as Layer from "effect/Layer";
+import { beforeEach, vi } from "vite-plus/test";
+
+const { appendSwitchMock, getSwitchValueMock, hasSwitchMock, registerSchemesMock } = vi.hoisted(
+  () => ({
+    appendSwitchMock: vi.fn(),
+    getSwitchValueMock: vi.fn(),
+    hasSwitchMock: vi.fn(),
+    registerSchemesMock: vi.fn(),
+  }),
+);
+
+vi.mock("electron", () => ({
+  app: {
+    commandLine: {
+      appendSwitch: appendSwitchMock,
+      getSwitchValue: getSwitchValueMock,
+      hasSwitch: hasSwitchMock,
+    },
+  },
+  protocol: {
+    registerSchemesAsPrivileged: registerSchemesMock,
+  },
+}));
+
+import * as DesktopPreReadyPlatform from "./DesktopPreReadyPlatform.ts";
+
+describe("DesktopPreReadyPlatform", () => {
+  beforeEach(() => {
+    appendSwitchMock.mockReset();
+    getSwitchValueMock.mockReset();
+    hasSwitchMock.mockReset();
+    registerSchemesMock.mockReset();
+  });
+
+  it("reads an explicit Electron command-line switch value", () => {
+    const value = DesktopPreReadyPlatform.readCommandLineSwitchValue(
+      {
+        hasSwitch: (switchName) => switchName === "password-store",
+        getSwitchValue: (switchName) => {
+          assert.equal(switchName, "password-store");
+          return "basic";
+        },
+      },
+      "password-store",
+    );
+
+    assert.equal(value, "basic");
+  });
+
+  it("treats valueless Electron command-line switches as absent", () => {
+    const value = DesktopPreReadyPlatform.readCommandLineSwitchValue(
+      {
+        hasSwitch: () => true,
+        getSwitchValue: () => "",
+      },
+      "password-store",
+    );
+
+    assert.isNull(value);
+  });
+
+  it("returns null for missing Electron command-line switches", () => {
+    const value = DesktopPreReadyPlatform.readCommandLineSwitchValue(
+      {
+        hasSwitch: () => false,
+        getSwitchValue: () => {
+          throw new Error("Unexpected switch value read.");
+        },
+      },
+      "password-store",
+    );
+
+    assert.isNull(value);
+  });
+
+  it.effect(
+    "acquires a synchronous pre-ready layer before an asynchronous Clerk-shaped layer",
+    () =>
+      Effect.gen(function* () {
+        class ClerkShaped extends Context.Service<ClerkShaped, { readonly ready: true }>()(
+          "@shuv2code/desktop/app/DesktopPreReadyPlatform.test/ClerkShaped",
+        ) {}
+
+        const events: Array<string> = [];
+        registerSchemesMock.mockImplementation(() => {
+          events.push("pre-ready");
+        });
+
+        const preReadyLayer = DesktopPreReadyPlatform.layer.pipe(
+          Layer.provide(Layer.succeed(HostProcessPlatform, "darwin")),
+        );
+
+        const clerkShapedLayer = Layer.effect(
+          ClerkShaped,
+          Effect.promise(() => Promise.resolve()).pipe(
+            Effect.map(() => {
+              events.push("clerk");
+              return { ready: true as const };
+            }),
+          ),
+        );
+
+        const runtimeLayer = clerkShapedLayer.pipe(
+          Layer.flatMap((clerkContext) => Layer.succeedContext(clerkContext)),
+          Layer.provideMerge(preReadyLayer),
+        );
+
+        const result = yield* Effect.all({
+          clerk: ClerkShaped,
+          preReady: DesktopPreReadyPlatform.DesktopPreReadyElectronOptions,
+        }).pipe(Effect.provide(runtimeLayer));
+
+        assert.deepEqual(result, {
+          clerk: { ready: true },
+          preReady: {
+            linux: null,
+            linuxPasswordStoreCommandLine: null,
+          },
+        });
+        assert.deepEqual(events, ["pre-ready", "clerk"]);
+        assert.equal(registerSchemesMock.mock.calls.length, 1);
+        assert.equal(appendSwitchMock.mock.calls.length, 0);
+      }),
+  );
+});

apps/desktop/src/electron/ElectronSafeStorage.ts
@@ -1,9 +1,11 @@
 import * as Context from "effect/Context";
 import * as Effect from "effect/Effect";
 import * as Layer from "effect/Layer";
+import * as Option from "effect/Option";
 import * as Schema from "effect/Schema";
 
 import * as Electron from "electron";
+import { HostProcessPlatform } from "@shuv2code/shared/hostProcess";
 
 const electronSafeStorageErrorFields = {
   cause: Schema.Defect(),
@@ -60,24 +62,39 @@ export class ElectronSafeStorage extends Context.Service<
     readonly decryptString: (
       value: Uint8Array,
     ) => Effect.Effect<string, ElectronSafeStorageDecryptError>;
+    readonly selectedStorageBackend: Effect.Effect<Option.Option<string>>;
   }
 >()("@shuv2code/desktop/electron/ElectronSafeStorage") {}
 
-export const make = ElectronSafeStorage.of({
-  isEncryptionAvailable: Effect.try({
-    try: () => Electron.safeStorage.isEncryptionAvailable(),
-    catch: (cause) => new ElectronSafeStorageAvailabilityError({ cause }),
-  }),
-  encryptString: (value) =>
-    Effect.try({
-      try: () => Electron.safeStorage.encryptString(value),
-      catch: (cause) => new ElectronSafeStorageEncryptError({ cause }),
+export const make = Effect.gen(function* () {
+  const platform = yield* HostProcessPlatform;
+
+  return ElectronSafeStorage.of({
+    isEncryptionAvailable: Effect.try({
+      try: () => Electron.safeStorage.isEncryptionAvailable(),
+      catch: (cause) => new ElectronSafeStorageAvailabilityError({ cause }),
     }),
-  decryptString: (value) =>
-    Effect.try({
-      try: () => Electron.safeStorage.decryptString(Buffer.from(value)),
-      catch: (cause) => new ElectronSafeStorageDecryptError({ cause }),
+    encryptString: (value) =>
+      Effect.try({
+        try: () => Electron.safeStorage.encryptString(value),
+        catch: (cause) => new ElectronSafeStorageEncryptError({ cause }),
+      }),
+    decryptString: (value) =>
+      Effect.try({
+        try: () => Electron.safeStorage.decryptString(Buffer.from(value)),
+        catch: (cause) => new ElectronSafeStorageDecryptError({ cause }),
+      }),
+    selectedStorageBackend: Effect.sync(() => {
+      if (platform !== "linux") {
+        return Option.none();
+      }
+      try {
+        return Option.fromNullishOr(Electron.safeStorage.getSelectedStorageBackend());
+      } catch {
+        return Option.none();
+      }
     }),
+  });
 });
 
-export const layer = Layer.succeed(ElectronSafeStorage, make);
+export const layer = Layer.effect(ElectronSafeStorage, make);